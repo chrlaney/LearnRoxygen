10ce5b50fb40feb973837bf00a924c13f8bde792
Christine Laney <claney@neoninc.org>
2015-01-30 15:01:46 -0700

    changed to LearnRoxygen

---
+++ R/<LearnRoxygen>.R
@@ -0,0 +1,13 @@
+#' LearnRoxygen: A package for understanding the roxygen2 package.
+#'
+#' The LearnRoxygen package provides many functions!
+#' 
+#' @section Data parsing functions:
+#' Functions to parse data ...
+#'
+#' @section Data aggregating functions:
+#' Functions to aggregate data ...
+#'
+#' @docType package
+#' @name LearnRoxygen
+NULL
